5fd5a5bdc814b30147b502f2f877e1193e816c9e
Dillon Brock <mdillon1297@gmail.com>
2022-09-19 15:22:15 -0700

    working continent dropdown

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,8 +3,11 @@ import './Main.css';
 import { useEffect, useState } from 'react';
 import { fetchCountries } from '../../services/countries';
 import CountryCard from '../CountryCard/CountryCard';
+import Filter from '../Filter/Filter';
 
 export default function Main() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [continent, setContinent] = useState('');
   const [countries, setCountries] = useState([]);
 
   useEffect(() => {
@@ -12,12 +15,19 @@ export default function Main() {
       const data = await fetchCountries();
       setCountries(data);
     }
-    fetchData();
+    fetchData(searchTerm);
   }, []);
+
+  const filterCountries = () => {
+    return continent ? countries.filter((country) => country.continent === continent) : countries;
+  };
 
   return (
     <main>
-      {countries.map(country => <CountryCard key={country.id} { ...country } />)}
+      <Filter {...{ searchTerm, setSearchTerm, continent, setContinent }}/>
+      <div id='countries'>
+        {filterCountries().map(country => <CountryCard key={country.id} { ...country } />)}
+      </div>
     </main>
   );
 }

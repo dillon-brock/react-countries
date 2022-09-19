532c2b6e9c5e5b255990b1433d7fbafd001681b7
Dillon Brock <mdillon1297@gmail.com>
2022-09-19 14:43:40 -0700

    country names and flags displaying, no filtering or searching implemented and no styling

--- src/components/CountryCard/CountryCard.js
+++ src/components/CountryCard/CountryCard.js
@@ -2,7 +2,7 @@ export default function CountryCard({ name, iso2 }) {
   return (
     <div className="country">
       <h3>{name}</h3>
-      <img src={`https://flagcdn.com/72x54/${iso2}.png`}/>
+      <img src={`https://flagcdn.com/72x54/${iso2.toLowerCase()}.png`}/>
     </div>
-  )
+  );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,21 @@
+import { useEffect, useState } from 'react';
 import { fetchCountries } from '../../services/countries';
+import CountryCard from '../CountryCard/CountryCard';
 
 export default function Main() {
-  fetchCountries();
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetchCountries();
+      setCountries(data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <main>
+      {countries.map(country => <CountryCard key={country.id} { ...country } />)}
+    </main>
+  );
 }
